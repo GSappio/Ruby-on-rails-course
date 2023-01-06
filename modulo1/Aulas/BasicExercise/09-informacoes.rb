f886850a14051592f2ed72c84a5148c87552e243
GabrielSappio <gabriel.sappio02@gmail.com>
2023-01-06 10:26:44 -0300

    About parentheses and constants

---
+++ modulo1/Aulas/BasicExercise/09-informacoes.rb
@@ -0,0 +1,12 @@
+# O uso de parentese é opcional. Funcionará da mesma forma.
+puts "Gabriel"
+puts ("Gabriel")
+
+# Constantes são usadas para representar valores
+# Constantes não alteram o valor, boa para usar em valores 
+# que ficaram sem alterar até o final da sua aplicação
+
+NOME = "Abacaxi" 
+PI = 3.14
+puts NOME
+puts PI
