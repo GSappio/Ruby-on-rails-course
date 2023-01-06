aa100eb8a7c87dc5ed0ad7a0569d5a7c5d1bfffe
GabrielSappio <gabriel.sappio02@gmail.com>
2023-01-06 10:13:28 -0300

    Learning to use Symbols

---
+++ modulo1/Aulas/BasicExercise/08-symbols.rb
@@ -0,0 +1,21 @@
+puts "Gabriel".object_id
+puts "Gabriel".object_id
+puts "Gabriel".object_id
+puts "Gabriel".object_id
+puts "Gabriel".object_id
+
+puts ("_______________________________") 
+puts "\n"
+
+# Usando Symbols -> : ele vai usar o mesmo endereço de memoria, o mesmo ID
+# Hashes adoram Symbols.
+puts :Gabriel.object_id
+puts :Gabriel.object_id
+puts :Gabriel.object_id
+puts :Gabriel.object_id
+puts :Gabriel.object_id
+
+puts ("_______________________________") 
+puts "\n"
+
+
