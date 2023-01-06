296910d336db9d2d95a8bcef06d70db668762cd9
GabrielSappio <gabriel.sappio02@gmail.com>
2023-01-06 08:25:50 -0300

    Repeating structures: while and each

---
+++ modulo1/Aulas/BasicExercise/04-repeticao.rb
@@ -0,0 +1,23 @@
+# EACH EX-02
+nome = 'Gabriel'
+
+nome.each_char do |letra|
+  puts letra
+end
+
+# EACH EX-01
+(0..5).each do |i|
+    puts "O valor lido foi: " + i.to_s
+end
+
+# WHILE
+i = 0
+num =  5
+
+while i < num do
+    puts "Contando..." + i.to_s
+    i += 1
+end
+
+
+
