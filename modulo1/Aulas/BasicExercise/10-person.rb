d691880c8451d6b096782c1cfb9b6c6f6c4bdc90
GabrielSappio <gabriel.sappio02@gmail.com>
2023-01-06 11:22:52 -0300

    Creating class and methods

---
+++ modulo1/Aulas/BasicExercise/10-person.rb
@@ -0,0 +1,20 @@
+class Person # Criando class Person.
+    def speak # Criando metodo speak.
+        "Hello, people!!" # Retorno do método, o que irá fazer.
+    end # Fechamento.
+end
+
+# 01-) No terminal foi usado os seguintes códigos:
+
+# > sudo pry
+# > require_relative "10-person.rb"
+# > p = Person.new
+# > p.class
+
+# 02-) No terminal foi usado os seguintes códigos:
+
+# > sudo pry
+# > require_relative "10-person.rb"
+# > p = Person.new
+# > puts p.speak 
+# => Hello, people!!
