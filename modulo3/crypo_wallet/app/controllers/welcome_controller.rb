4b52ba676de95f0b362ebfedb62f310998e74ce6
GabrielSappio <gabriel.sappio02@gmail.com>
2023-01-13 12:31:58 -0300

    Cookies and Sessions

--- modulo3/crypo_wallet/app/controllers/welcome_controller.rb
+++ modulo3/crypo_wallet/app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    cookies[:curso] = "Curso de Ruby on Rails - Gabriel Sappio[COOKIE]"
+    session[:curso] = "Curso de Ruby on Rails - Gabriel Sappio[SESSION]"
     @meu_nome = params[:nome] 
     @curso = params[:curso]
   end
