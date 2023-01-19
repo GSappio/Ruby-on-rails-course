42836f93ff8f406fc94119c5c8f2bb8887b47eec
GabrielSappio <gabriel.sappio02@gmail.com>
2023-01-19 08:46:03 -0300

    Adjusting the layout system (website)

--- modulo4/time_2/app/controllers/welcome_controller.rb
+++
@@ -1,6 +0,0 @@
-class WelcomeController < ApplicationController
-  # http_basic_authenticate_with name: "Gabriel", password: "123"
-
-  def index 
-  end
-end
--- modulo4/time_2/config/routes.rb
+++ modulo4/time_2/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :site do
+    get 'welcome/index'
+  end
   namespace :profiles_backoffice do
     get 'welcome/index'
   end
@@ -8,10 +11,10 @@ Rails.application.routes.draw do
   devise_for :profiles
   devise_for :admins
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'welcome/index'
-  get 'inicio', to: 'welcome#index'
+  
+  get 'inicio', to: 'site/welcome#index'
 
-  root to: 'welcome#index'
+  root to: 'site/welcome#index'
   
 end
 
