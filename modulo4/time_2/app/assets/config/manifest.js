acf108d11720c4f9703bc358ec80e378fc25f5b0
GabrielSappio <gabriel.sappio02@gmail.com>
2023-01-23 09:32:16 -0300

    Porting the Gentelella theme to User #01

--- modulo4/time_2/app/assets/config/manifest.js
+++ modulo4/time_2/app/assets/config/manifest.js
@@ -1,2 +1,7 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+
+//= require jquery/dist/jquery
+//= require bootstrap/dist/js/bootstrap
+//= require metismenu/dist/metisMenu
+//= sb-admin-2
