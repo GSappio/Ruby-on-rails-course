33d2341db13638d39d772a0653962e7c51a86857
GabrielSappio <gabriel.sappio02@gmail.com>
2023-01-24 12:56:38 -0300

    Deleting an admin

--- modulo4/time_2/app/controllers/admins_backoffice/admins_controller.rb
+++ modulo4/time_2/app/controllers/admins_backoffice/admins_controller.rb
@@ -1,7 +1,7 @@
 class AdminsBackoffice::AdminsController < AdminsBackofficeController
 
   before_action :verify_password, only: [:update]
-  before_action :set_admin, only: [:edit, :update]
+  before_action :set_admin, only: [:edit, :update, :destroy]
 
   def index
     @admins = Admin.all
@@ -24,11 +24,18 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
   end
 
   def update
-
     if @admin.update(params_admin)
       redirect_to admins_backoffice_admins_path, notice: "Administrador atualizado com sucesso!"  
     else 
       render :edit
+    end
+  end
+ 
+  def destroy 
+    if @admin.destroy
+      redirect_to admins_backoffice_admins_path, notice: "Administrador excluído com sucesso!"  
+    else 
+      render :index
     end
   end
 
