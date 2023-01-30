e055c0710ad43fdfc816dc18e4ce32b5ddc0e772
GabrielSappio <gabriel.sappio02@gmail.com>
2023-01-30 09:05:03 -0300

    Installing and configuring the Cocoon gem #02

--- modulo4/time_2/app/controllers/admins_backoffice/subjects_controller.rb
+++ modulo4/time_2/app/controllers/admins_backoffice/subjects_controller.rb
@@ -1,5 +1,4 @@
 class AdminsBackoffice::SubjectsController < AdminsBackofficeController
-
     before_action :set_subject, only: [:edit, :update, :destroy]
   
     def index
@@ -12,9 +11,9 @@ class AdminsBackoffice::SubjectsController < AdminsBackofficeController
   
     def create
       @subject = Subject.new(params_subject)
-      if @subject.save
-        redirect_to admins_backoffice_subjects_path, notice: "Assunto/Área cadastrado com sucesso!"  
-      else 
+      if  @subject.save
+        redirect_to admins_backoffice_subjects_path, notice: "Assunto/Área cadastrado com sucesso!"
+      else
         render :new
       end
     end
@@ -22,30 +21,29 @@ class AdminsBackoffice::SubjectsController < AdminsBackofficeController
     def edit
     end
   
-    def update
-      if @subject.update(params_subject)
-        redirect_to admins_backoffice_subjects_path, notice: "Assunto/Área atualizado com sucesso!"  
-      else 
+    def update    
+      if  @subject.update(params_subject)
+        redirect_to admins_backoffice_subjects_path, notice: "Assunto/Área atualizado com sucesso!"
+      else
         render :edit
       end
     end
-   
-    def destroy 
-      if @subject.destroy
-        redirect_to admins_backoffice_subjects_path, notice: "Assunto/Área excluído com sucesso!"  
-      else 
+  
+    def destroy
+      if  @subject.destroy
+        redirect_to admins_backoffice_subjects_path, notice: "Assunto/Área excluído com sucesso!"
+      else
         render :index
       end
     end
   
     private
-  
-    def params_subject 
-      params_subject = params.require(:subject).permit(:description)
+    
+    def params_subject
+      params.require(:subject).permit(:description)
     end
   
     def set_subject
       @subject = Subject.find(params[:id])
     end
   end
-  
--- modulo4/time_2/app/controllers/application_controller.rb
+++ modulo4/time_2/app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    layout :layout_by_resource
+
+    private
+
+    def layout_by_resource
+        puts ">>>>>>>>>>> #{devise_controller?}" 
+        puts ">>>>>>>>>>> #{resource_class}"
+        "application"       
+    end
 end
