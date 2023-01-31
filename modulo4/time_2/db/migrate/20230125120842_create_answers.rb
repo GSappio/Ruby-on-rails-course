afc793a2d468efd34e2f08c3f4f20dc681ec922a
GabrielSappio <gabriel.sappio02@gmail.com>
2023-01-31 10:54:38 -0300

    Listing the questions and answers in the index

--- modulo4/time_2/db/migrate/20230125120842_create_answers.rb
+++ modulo4/time_2/db/migrate/20230125120842_create_answers.rb
@@ -1,8 +1,8 @@
 class CreateAnswers < ActiveRecord::Migration[5.2]
   def change
     create_table :answers do |t|
-      t.references :question, foreign_key: true
       t.text :description,  null: false
+      t.references :question, foreign_key: true
       t.boolean :correct, default: false
 
       t.timestamps
