7b77bde71be05b763737bea9460216984a8b245a
GabrielSappio <gabriel.sappio02@gmail.com>
2023-01-31 12:34:20 -0300

    Using class methods in the model

--- modulo4/time_2/app/models/question.rb
+++ modulo4/time_2/app/models/question.rb
@@ -4,4 +4,14 @@ class Question < ApplicationRecord
   accepts_nested_attributes_for :answers, reject_if: :all_blank, allow_destroy: true
   
   paginates_per 5 
+
+  def self.search(page, term)
+    Question.includes(:answers)
+      .where("lower (description) LIKE ?", "%#{term.downcase}%")
+      .page(page)
+  end
+
+  def self.last_questions(page)
+    Question.includes(:answers).order('created_at desc').page(page)
+  end
 end
