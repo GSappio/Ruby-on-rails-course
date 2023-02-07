b9dce682c7513118969fd6085a253b11b53531ba
GabrielSappio <gabriel.sappio02@gmail.com>
2023-02-07 08:30:36 -0300

    Working with PDF and responding to HTML and JSON formats

--- modulo4/time_2/app/controllers/application_controller.rb
+++ modulo4/time_2/app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   layout :layout_by_resource
   before_action :check_pagination
-  
+  before_action :set_global_params
+
   protected
   
   def layout_by_resource
@@ -9,8 +10,12 @@ class ApplicationController < ActionController::Base
   end
 
   def check_pagination
-    unless user_signed_in?
+    unless user_signed_in? or admin_signed_ind?
       params.extract!(:page)
     end
+  end
+
+  def set_global_params
+    $global_params = params
   end
 end
